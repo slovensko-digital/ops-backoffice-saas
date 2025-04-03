e246a2168e7bdc715e095c38f5db5155255b555b
Marek Ceľuch <celuchmarek@gmail.com>
2025-04-03 13:20:11 +0200

    change ops_issue_type to select resolves #248

---
+++ db/migrate/20250403110027_ops_change_ops_issue_type_to_select.rb
@@ -0,0 +1,51 @@
+class OpsChangeOpsIssueTypeToSelect < ActiveRecord::Migration[7.0]
+  def up
+    return unless Setting.exists?(name: 'system_init_done')
+
+    if ObjectManager::Attribute.where(name: 'ops_issue_type', object_lookup: ObjectLookup.by_name('Ticket')).exists?
+      ObjectManager::Attribute.remove(name: 'ops_issue_type', object: 'Ticket')
+      ObjectManager::Attribute.migration_execute
+    end
+
+    ObjectManager::Attribute.add(
+      object: 'Ticket',
+      name: 'ops_issue_type',
+      display: 'Typ dopytu',
+      data_type: 'select',
+      data_option: {
+        options: [
+          { name: 'Podnet', value: 'issue' },
+          { name: 'Otázka', value: 'question' },
+          { name: 'Pochvala', value: 'praise' }
+        ],
+        customsort: 'on',
+        default: '',
+        null: true,
+        nulloption: true,
+        maxlength: 255,
+      },
+      active: true,
+      screens: {
+        create_middle: {
+          'ticket.agent' => { shown: false },
+          'ticket.customer' => { shown: false }
+        },
+        edit: {
+          'ticket.agent' => { shown: false },
+          'ticket.customer' => { shown: false }
+        }
+      },
+      position: 22,
+      created_by_id: 1,
+      updated_by_id: 1
+    )
+
+    ObjectManager::Attribute.migration_execute
+
+    Ticket.where(origin: 'ops').find_each { |ticket| ticket.update(ops_issue_type: "issue") }
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, 'Cannot revert ops_issue_type to string'
+  end
+end
